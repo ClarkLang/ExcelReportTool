657971411fcbe2fa806a71542c2ff03b10a84235
964842443@qq.com <clark.lang@sap.com>
2017-02-14 18:43:33 +0800

    check in

--- src/versionone/utils/UI.java
+++ src/versionone/utils/UI.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import jxl.Workbook;
+import jxl.format.UnderlineStyle;
 import jxl.write.*;
 import versionone.bean.ColourBean;
 import versionone.utils.ExcelGeneratorUtils;
@@ -27,14 +28,28 @@ public class UI {
 		book = Workbook.createWorkbook(new File("c:/cdp/" + dataStr +".xls"));
 		
         WritableSheet sheet = book.createSheet("name", 0);
-        Label label = new Label(0, 0, "clark");
+        Label label = new Label(0, 1, "clark");
         sheet.addCell(label);
+        
+        WritableFont wf_title = new WritableFont(WritableFont.ARIAL, 11, WritableFont.NO_BOLD, false, UnderlineStyle.NO_UNDERLINE,  jxl.format.Colour.BLACK);
+        
+        WritableCellFormat wcf_title1 = new WritableCellFormat(wf_title);  
+        wcf_title1.setBackground(jxl.format.Colour.LIGHT_GREEN);  
+        wcf_title1.setAlignment(jxl.format.Alignment.CENTRE); 
+        wcf_title1.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN,jxl.format.Colour.BLACK); 
+        
+        Label labelName = new Label(0, 0, "Username");
+        Label labelTile01 = new Label(1, 0, "Title", wcf_title1);
+        sheet.addCell(labelName);
+        sheet.addCell(labelTile01);
         
         WritableCellFormat greyBackground = new WritableCellFormat();
         ColourBean colour = new ColourBean();
         greyBackground.setBackground(colour.getTaskColour());
         
-        jxl.write.Number number = new jxl.write.Number(1, 0, 30, greyBackground);
+        
+        
+        jxl.write.Number number = new jxl.write.Number(1, 1, 30, greyBackground);
         sheet.addCell(number);
         
         ExcelGeneratorUtils test = new ExcelGeneratorUtils();
